9daa9217fe0df28f14dc26e897c8e2e3201fd648
vanajathirumal <vanajathirumal994@gmail.com>
2024-10-22 19:14:21 +0530

    basic if else statement

--- basic_py_prg.py
+++ basic_py_prg.py
@@ -1,35 +1,51 @@
-# square numbers
-num=int(input("enter the square number :"))
-square_num=num**2
-print(f"the {num} square is {square_num}")
+# # square numbers
+# num=int(input("enter the square number :"))
+# square_num=num**2
+# print(f"the {num} square is {square_num}")
 
-# print last elemnt in list
-elements=eval(input("enter the list: "))
-last_element=elements[-1]
-print(f"the {last_element} is the last element in {elements}")
+# # print last elemnt in list
+# elements=eval(input("enter the list: "))
+# last_element=elements[-1]
+# print(f"the {last_element} is the last element in {elements}")
 
-# print element in index position enter by user
-list1=eval(input("enter the list items :"))
-index=int(input("enter the index position :"))
-item=list1[index]
-print(f"the element at index {index} is {item}")
+# # print element in index position enter by user
+# list1=eval(input("enter the list items :"))
+# index=int(input("enter the index position :"))
+# item=list1[index]
+# print(f"the element at index {index} is {item}")
 
-# remove element in index position enter by user
-list1=eval(input("enter the list items :"))
-index=int(input("enter the index position :"))
-list1.pop(index)
-print(f"the element at index {index} is removed from {list1}")
+# # remove element in index position enter by user
+# list1=eval(input("enter the list items :"))
+# index=int(input("enter the index position :"))
+# list1.pop(index)
+# print(f"the element at index {index} is removed from {list1}")
 
-# revese the word enter by user
-word=input("enter the word :")
-print(f"revrse of {word} is {word[::-1]}")
+# # revese the word enter by user
+# word=input("enter the word :")
+# print(f"revrse of {word} is {word[::-1]}")
 
-# basic math operation
-num1=int(input("enter the first number :"))
-num2=int(input("enter the second number :"))
-print(f"sum of {num1} and {num2} is {num1+num2}")        
-print(f"subtraction of {num1} and {num2} is {num1-num2}")
-print(f"multiplication of {num1} and {num2} is {num1*num2}")
-print(f"division of {num1} and {num2} is {num1/num2}")
-print(f"modules of {num1} and {num2} is {num1%num2}")
+# # basic math operation
+# num1=int(input("enter the first number :"))
+# num2=int(input("enter the second number :"))
+# print(f"sum of {num1} and {num2} is {num1+num2}")        
+# print(f"subtraction of {num1} and {num2} is {num1-num2}")
+# print(f"multiplication of {num1} and {num2} is {num1*num2}")
+# print(f"division of {num1} and {num2} is {num1/num2}")
+# print(f"modules of {num1} and {num2} is {num1%num2}")
 
+# set_a={1,2,3,4,6,8,12}
+# set_b={2,4,6,8}
+# result=set_b.issubset(set_a)
+# print(result)
+# set_a={1:'a',2:'b',3:"c"}
+# result=set_a.items()
+# print(f"{result} of items")
+def length(*words):
+    output={ }
+    for word in words:
+        if word not in output:
+            output[word]=len(word)
+    return output
+    
+print(length('hey','who','are','you'))
+    
